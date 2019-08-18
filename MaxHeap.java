2175c52dcff49689d5f43cff7c85e3c89a66b4d5
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-18 18:05:05 -0400

    working on the exchange method

--- MaxHeap.java
+++ MaxHeap.java
@@ -7,9 +7,20 @@ public class MaxHeap<T extends Comparable <T>> {
 
 	public void insert(T data) {
 
+		//checks if heap is empty
 		if (size == 0) {
 			list.add(data);
 		}
+		// adds to bottom first and looks to bubble up
+		else {
+			list.add(data);
+			// making an exchange method first
+		}
+	}
+
+	private void exchange(int one, int two) {
+		T temp = list.get(one);
+		list.insert()
 	}
 
 	public static void main(String[] args) {
