5c7982b2ce7d2450b7c8f790e11101a12816679c
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-17 23:21:55 -0400

    have to work on insert

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,16 +1,21 @@
 public class MaxHeap<T extends Comparable <T>> {
 
+	//head node of the MaxHeap
 	private Node root = null;
 
+	// max heap constructor
 	public MaxHeap(T data) {
 		this.root = new Node(data);
 	}
+
+	// innerclass Node for Heap class
 	class Node {
 
 		public Node(T data) {
 			this.data = data;
 		}
 
+		// data sotred inside a node
 		protected T data;
 		protected Node right;
 		protected Node left;
