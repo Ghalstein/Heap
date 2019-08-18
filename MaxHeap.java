6eaa8facf0a769e4aba2087f502c04b3a799fb32
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-17 23:15:10 -0400

    added the basic constructors for inner node class and MaxHeap

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,6 +1,23 @@
-public class MaxHeap {
+public class MaxHeap<T extends Comparable <T>> {
+
+	private Node root = null;
+
+	public MaxHeap(T data) {
+		this.root = new Node(data);
+	}
+	class Node {
+
+		public Node(T data) {
+			this.data = data;
+		}
+
+		protected T data;
+		protected Node right;
+		protected Node left;
+	}
+
 
 	public static void main(String[] args) {
-		
+
 	}
 }
