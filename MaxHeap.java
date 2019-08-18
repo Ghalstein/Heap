e29cbeb3d51936a313b7c15bf5ade8b5f8bb463a
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-18 18:06:50 -0400

    made the exchange method for the heap

--- MaxHeap.java
+++ MaxHeap.java
@@ -18,9 +18,12 @@ public class MaxHeap<T extends Comparable <T>> {
 		}
 	}
 
-	private void exchange(int one, int two) {
-		T temp = list.get(one);
-		list.insert()
+
+	// exchange helper method for two points of the heap
+	private void exchange(int first, int second) {
+		T temp = list.get(first);
+		list.add(first, list.get(second));
+		list.add(second, temp);
 	}
 
 	public static void main(String[] args) {
