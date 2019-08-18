164bab45af3a348570d7c02a81e50c454f05b2d1
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-18 18:01:07 -0400

    insertion for first node of heap made

--- MaxHeap.java
+++ MaxHeap.java
@@ -3,8 +3,16 @@ import java.util.*;
 public class MaxHeap<T extends Comparable <T>> {
 
 	private ArrayList<T> list = new ArrayList<>();
+	private int size = 0;
+
+	public void insert(T data) {
+
+		if (size == 0) {
+			list.add(data);
+		}
+	}
 
 	public static void main(String[] args) {
-		
+
 	}
 }
