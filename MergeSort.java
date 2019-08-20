c47769de829abbee28593720cb129acaa6671b9a
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-19 20:27:14 -0400

    unit tested toString on MertgeSort is passing

--- MergeSort.java
+++ MergeSort.java
@@ -1,6 +1,11 @@
 public class MergeSort {
 
 	public static void main(String[] args) {
-		
+		MaxHeap<Integer> heap = new MaxHeap<>();
+		heap.add(1);
+		heap.add(2);
+		heap.add(10);
+		heap.add(9);
+		System.out.println(heap);
 	}
 }
