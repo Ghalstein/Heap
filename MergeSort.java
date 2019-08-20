e405522c9358197995ce4b310a1abaf9499fdf75
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-20 07:45:05 -0400

    made the heap merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -8,9 +8,34 @@ public class MergeSort<T extends Comparable<T>> {
 		}
 	}
 
-	public void sort(ArrayList<T> l1, ArrayList<T> l2) {
+	public ArrayList<T> sort(ArrayList<T> l1, ArrayList<T> l2) {
 		MaxHeap<Integer> h1 = new MaxHeap<>();
 		MaxHeap<Integer> h2 = new MaxHeap<>();
+		ArrayList<T> list = ArrayList<>();
+		listToHeap(h1, l1);
+		listToHeap(h2, l2);
+		while (!h1.isEmpty() || !h2.isEmpty()) {
+			if (!h1.isEmpty() && !h2.isEmpty()) {
+				T h1Peek = h1.removeMax();
+				T h2Peek = h1.removeMax();
+				if (h1Peek.comapreTo(h2Peek) > 0) {
+					list.add(h1Peek);
+					list.add(h2Peek);
+				}
+				else {
+					list.add(h1Peek);
+					list.add(h1Peek);
+				}
+			}
+			else if (!h1.isEmpty()) {
+				list.add(h1Peek);
+			}
+			else {
+				list.add(h1Peek);
+			}
+		}
+
+		return list;
 	}
 
 	public static void main(String[] args) {
