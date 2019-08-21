a14d867618e4e31413dc85463bc2f3f5e74272ee
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-21 07:40:17 -0400

    made the find increment helper function

--- SortIncDec.java
+++ SortIncDec.java
@@ -1,10 +1,21 @@
-public class SortIncDec {
+public class SortIncDec<T extends Comparable <T>> {
 
-	private static int findIncrement() {
-		
+	private static int findIncrement(ArrayList<T> list) {
+		int count = 1;
+		int before = list.get(0)
+		while (count < list.size()) {
+			if (before < list.get(count)) {
+				before = list.get(count++);
+			}
+		}
+		return count;
 	}
 
 	public static void main(String[] args) {
-
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		System.out.println(findIncrement())
 	}
 }
