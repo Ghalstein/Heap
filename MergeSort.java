43fbb8a235a00d67c7be49c8c6b20ad0e1c2e3df
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-22 07:57:35 -0400

    first refactoring merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -15,17 +15,17 @@ public class MergeSort {
 		listToHeap(h1, l1);
 		listToHeap(h2, l2);
 		while (!h1.isEmpty() || !h2.isEmpty()) {
+			System.out.println("h1: " + h1);
+			System.out.println("h2: " + h2);
 			System.out.println(list);
 			if (!h1.isEmpty() && !h2.isEmpty()) {
 				int h1Peek = h1.removeMax();
 				int h2Peek = h2.removeMax();
 				if (h1Peek > h2Peek) {
 					list.add(h1Peek);
-					list.add(h2Peek);
 				}
 				else {
 					list.add(h2Peek);
-					list.add(h1Peek);
 				}
 			}
 			else if (!h1.isEmpty()) {
@@ -52,6 +52,12 @@ public class MergeSort {
 		l1.add(15);
 		l1.add(1);
 		l1.add(8);
+		l1.add(7);
+		l1.add(9);
+		l1.add(16);
+		l1.add(13);
+		l1.add(11);
+		l1.add(20);
 		ArrayList<Integer> l2 = new ArrayList<>();
 		l2.add(2);
 		l2.add(1);
