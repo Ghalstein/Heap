78b2e9a4e83e4fa0590617d80987565fd5f2b4d9
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-22 08:04:15 -0400

    more refactor needed

--- MergeSort.java
+++ MergeSort.java
@@ -15,8 +15,8 @@ public class MergeSort {
 		listToHeap(h1, l1);
 		listToHeap(h2, l2);
 		while (!h1.isEmpty() || !h2.isEmpty()) {
-			System.out.println("h1: " + h1);
-			System.out.println("h2: " + h2);
+			// System.out.println("h1: " + h1);
+			// System.out.println("h2: " + h2);
 			System.out.println(list);
 			if (!h1.isEmpty() && !h2.isEmpty()) {
 				int h1Peek = h1.removeMax();
@@ -52,12 +52,12 @@ public class MergeSort {
 		l1.add(15);
 		l1.add(1);
 		l1.add(8);
-		l1.add(7);
-		l1.add(9);
-		l1.add(16);
-		l1.add(13);
-		l1.add(11);
-		l1.add(20);
+		// l1.add(7);
+		// l1.add(9);
+		// l1.add(16);
+		// l1.add(13);
+		// l1.add(11);
+		// l1.add(20);
 		ArrayList<Integer> l2 = new ArrayList<>();
 		l2.add(2);
 		l2.add(1);
