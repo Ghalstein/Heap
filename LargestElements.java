8b889a0356f563a2286bf837784dca5443f13b22
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-23 19:15:45 -0400

    working on compute method

--- LargestElements.java
+++ LargestElements.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class LargestElements {
 
-		
+	public static ArrayList<Integer> compute(int k, ArrayList<Integer> arr) {
+		ArrayList<Integer> largest = new ArrayList<>();
+		return largest;
+	}
 
 	public static void main(String[] args) {
 
