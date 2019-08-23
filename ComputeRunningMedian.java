e604dcf430e5bf1987f281bc41e8274ec2aeef1e
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-23 07:48:49 -0400

    unit testing

--- ComputeRunningMedian.java
+++ ComputeRunningMedian.java
@@ -17,5 +17,14 @@ public class ComputeRunningMedian {
 
 	public static void main(String[] args) {
 
+		ArrayList<Double> list = new ArrayList<>();
+		list.add(1);
+		list.add(0);
+		list.add(3);
+		list.add(5);
+		list.add(2);
+		list.add(0);
+		list.add(1);
+		System.out.println()
 	}
 }
