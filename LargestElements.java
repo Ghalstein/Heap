c03bff5e698a2530628509b8f34dc2fba7264fe5
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-23 19:18:12 -0400

    prelim done

--- LargestElements.java
+++ LargestElements.java
@@ -2,8 +2,16 @@ import java.util.*;
 
 public class LargestElements {
 
+	// finds the k largest lelemtns in an array
 	public static ArrayList<Integer> compute(int k, ArrayList<Integer> arr) {
 		ArrayList<Integer> largest = new ArrayList<>();
+		MaxHeap<Integer> heap = new MaxHeap<>();
+		for (int curr : arr) {
+			heap.add(curr);
+		}
+		for (int i = 0; i < k; ++i) {
+			largest.add(heap.removeMax());
+		}
 		return largest;
 	}
 
